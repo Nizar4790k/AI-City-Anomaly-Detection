52625fcf667aa5441c881a4328392b0e118bef67
Nizar <nizar4790k@gmail.com>
2021-06-04 10:07:09 +0000

    capture_and_average.py is now working

--- src/bg_modeling/capture_and_average.py
+++ src/bg_modeling/capture_and_average.py
@@ -3,16 +3,37 @@ import os
 import sys
 import glob
 import tqdm
+from pathlib import Path
+from natsort import natsorted
+import numpy as np
+import json
 # video_name = sys.argv[1]
-root = "data/AIC_Track3/videos/"
-dest_dir = "data/AIC_Track3/ori_images/"
-video_names = [str(i)+'.mp4' for i in range(1,101)]
+
+dest_dir = "Data/ori_images/"
+
+def get_videos():
+  
+  
+  with open('Data/dataset.json', 'r') as f:
+    data = json.load(f)
+
+  
+  videos = data['videos']
+
+  return videos  
+
+
+ori_images_txt = open("ori_images.txt","w")
+videos = get_videos()
+
 print("caputure videos")
-for video_name in tqdm.tqdm(video_names):
-    file_name = video_name
+for video in tqdm.tqdm(videos):
+    file_name = video['name'].split('.')[0]
     folder_name = dest_dir+file_name.split('.')[0]
+    
     os.makedirs(folder_name,exist_ok=True)
-    vc = cv2.VideoCapture(root+video_name)
+    vc = cv2.VideoCapture(video['path'])
+    
     c = 1
     if vc.isOpened():  # 判断是否正常打开
         rval, frame = vc.read()
@@ -24,37 +45,47 @@ for video_name in tqdm.tqdm(video_names):
     while rval:  # 循环读取视频帧
         rval, frame = vc.read()
         pic_path = folder_name+'/'
-        if (c % timeF == 0):  # 每隔timeF帧进行存储操作
+        if (c % timeF == 0 and rval):  # 每隔timeF帧进行存储操作
             # print(pic_path + str(c) + '.jpg')
             cv2.imwrite(pic_path + str(c) + '.jpg', frame)  # 存储为图像,保存名为 >文件夹名_数字（第几个文件）.jpg
+            ori_images_txt.write("../AI-City-Anomaly-Detection/"+pic_path+str(c)+'.jpg')
+            ori_images_txt.write('\n')
+        
         c = c + 1
         cv2.waitKey(1)
     vc.release()
 
-dest_dir_processed = "data/AIC_Track3/processed_images/"
+dest_dir_processed = "Data/processed_images/"
+
 print("average images")
-for i in tqdm.tqdm(range(1,101)):
-    video_name = str(i)
-    path_file_number=glob.glob(os.path.join(dest_dir,video_name,'*.jpg')) #获取当前文件夹下个数
+
+for video in tqdm.tqdm(videos):
+    video_name = video['name'].split('.')[0]
+    path_file_number=glob.glob(os.path.join(dest_dir,video_name,'*.jpg')) 
     internal_frame = 4
-    start_frame = 1
-    video_name = str(i)
+    start_frame = 100
+   
     nums_frames = len(path_file_number)
     alpha=0.1
-    os.mkdir(dest_dir_processed+video_name,exist_ok=True)
+    Path(dest_dir_processed+video_name).mkdir(exist_ok=True)
 
     for j in range(4,5):
-        internal_frame = 4+j*4
-        num_pic = int(nums_frames/internal_frame)
-        former_im = cv2.imread(dest_dir_processed+"%d/1.jpg"%i)
-        img = cv2.imread(os.path.join(root,video_name,str(start_frame)+'.jpg'))
+        internal_frame = 100
+        num_pic = int(nums_frames)
+        former_im = cv2.imread(dest_dir+video_name+"/100.jpg")
+        
+        img = cv2.imread(os.path.join(dest_dir,video_name,str(start_frame)+'.jpg'))
         for i in range(num_pic):
-            now_im = cv2.imread(os.path.join(root,video_name,str(i*internal_frame+start_frame)+'.jpg'))
-            if np.mean(np.abs(now_im-former_im))>5:
-                img = img*(1-alpha)+now_im*alpha
-                cv2.imwrite(dest_dir_processed+video_name+'/'+str(i*internal_frame+start_frame)
+            # print(os.path.join(dest_dir,video_name,str(start_frame)+'.jpg'))
+            # print(os.path.join(dest_dir,video_name,str(i*internal_frame+start_frame)+'.jpg'))
+            now_im = cv2.imread(os.path.join(dest_dir,video_name,str(i*internal_frame+start_frame)+'.jpg'))
+            if now_im is not None:
+              if np.mean(np.abs(now_im-former_im))>5:
+                  img = img*(1-alpha)+now_im*alpha
+                  cv2.imwrite(dest_dir_processed+video_name+'/'+str(i*internal_frame+start_frame)
                         +'_'+str(j)+'.jpg',img)
-            else:
-                cv2.imwrite(dest_dir_processed+video_name+'/'+str(i*internal_frame+start_frame)
+              else:
+                  cv2.imwrite(dest_dir_processed+video_name+'/'+str(i*internal_frame+start_frame)
                         +'_'+str(j)+'.jpg',img*0)
-            former_im = now_im
+              former_im = now_im
+
